9c53c0435b28d646404e44de5995f36341ee9310
DhanashriKhade125 <coder.dhanashrikhade12@gmail.com>
2022-12-30 23:24:07 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              int Aprice, Sprice, Profit,Loss;
+
+            Console.WriteLine(" Enter actual Price: ");
+              Aprice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Enter Selling Price: ");
+              Sprice = Convert.ToInt32(Console.ReadLine());
+
+            if (Sprice > Aprice)
+            {
+                Profit = Sprice - Aprice;
+                Console.Write("Profit");
+            }
+            else (Aprice > Sprice)
+            {
+                Loss = Aprice - Sprice;
+                Console.Write("Loss");
+            }
+        }
+    }
+}
